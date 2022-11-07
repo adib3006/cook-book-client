837b1112007d6fc2ef59f293e4ff7a59e80341e2
adib3006 <ma.adib3006@gmail.com>
2022-11-08 02:58:47 +0600

    git initialized

---
+++ src/Router/Router/Routes.js
@@ -0,0 +1,26 @@
+import Main from '../../Layouts/Main';
+import Home from '../../components/Home/Home';
+import Login from '../../components/Login/Login';
+
+const { createBrowserRouter } = require("react-router-dom");
+
+export const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/home',
+                element:<Home></Home>
+            },
+            {
+                path:'/login',
+                element:<Login></Login>
+            }
+        ]
+    }
+])
