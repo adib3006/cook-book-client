5ebbdbc176c3a1d01d67f4dcfa8a0c029b1030a3
adib3006 <ma.adib3006@gmail.com>
2022-11-09 03:26:52 +0600

    photo viewer added

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { FaRegStar } from 'react-icons/fa';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const Details = () => {
     const item = useLoaderData();
     const { title, img, ratings, price, description } = item;
     return (
-        <div>
-            <div className='m-10'>
-                <div className='flex justify-center mb-4'>
-                    <img className='rounded-xl' src={img} alt="" />
+        <PhotoProvider>
+            <div>
+                <div className='m-10'>
+                    <div className='flex justify-center mb-4'>
+                        <PhotoView key={item.item_id} src={img}><img className='rounded-xl' src={img} alt="" />
+                        </PhotoView>
+                    </div>
+                    <h1 className='text-5xl text-center font-bold mb-6'>{title}</h1>
+                    <p className='text-2xl font-semibold mb-4'>Price : ${price}</p>
+                    <p className='text-2xl font-semibold flex items-center mb-4'>Ratings : {ratings}<span className='text-orange-500'><FaRegStar /></span></p>
+                    <p className='text-2xl font-semibold mb-4'>Description : {description}</p>
                 </div>
-                <h1 className='text-5xl text-center font-bold mb-6'>{title}</h1>
-                <p className='text-2xl font-semibold mb-4'>Price : ${price}</p>
-                <p className='text-2xl font-semibold flex items-center mb-4'>Ratings : {ratings}<span className='text-orange-500'><FaRegStar/></span></p>
-                <p className='text-2xl font-semibold mb-4'>Description : {description}</p>
             </div>
-        </div>
+        </PhotoProvider>
     );
 };
 
--- src/components/MenuPage/MenuPageItems.js
+++ src/components/MenuPage/MenuPageItems.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {FaRegStar} from 'react-icons/fa';
+import { FaRegStar } from 'react-icons/fa';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
-const MenuPageItems = ({menuItem}) => {
+const MenuPageItems = ({ menuItem }) => {
     const { item_id, img, title, price, ratings, description } = menuItem;
     let cardDescription = '';
-    if(description.length > 100){
-        cardDescription = description.slice(0,100);
+    if (description.length > 100) {
+        cardDescription = description.slice(0, 100);
     }
-    else{
+    else {
         cardDescription = description;
     }
     return (
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src={img} alt="Shoes" /></figure>
-            <div className="card-body">
-                <div className='flex items-center justify-between'>
-                    <h2 className="card-title">{title}</h2>
-                    <div className="badge badge-warning">{ratings} <FaRegStar/></div>
-                </div>
-                <p className='text-2xl font-semibold text-orange-600'>Price : ${price}</p>
-                <p>Description: {cardDescription}</p>
-                <div className="card-actions justify-end">
-                    <Link to={`/menu/${item_id}`}>
-                        <button className="btn btn-error">View Details</button>
-                    </Link>
+        <PhotoProvider>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <PhotoView key={item_id} src={img}><figure><img src={img} alt="Shoes" /></figure></PhotoView>
+                <div className="card-body">
+                    <div className='flex items-center justify-between'>
+                        <h2 className="card-title">{title}</h2>
+                        <div className="badge badge-warning">{ratings} <FaRegStar /></div>
+                    </div>
+                    <p className='text-2xl font-semibold text-orange-600'>Price : ${price}</p>
+                    <p>Description: {cardDescription}</p>
+                    <div className="card-actions justify-end">
+                        <Link to={`/menu/${item_id}`}>
+                            <button className="btn btn-error">View Details</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
+        </PhotoProvider>
     );
 };
 
