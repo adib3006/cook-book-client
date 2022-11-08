49bc03848491fc9c3fa9ad280df30afe2b429058
adib3006 <ma.adib3006@gmail.com>
2022-11-09 01:51:06 +0600

    authentication functionality created

---
+++ src/contexts/AuthProvider.js
@@ -0,0 +1,59 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile} from 'firebase/auth';
+import app from './../firebase/firebase.config';
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null);
+    const [loading,setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email,password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const updateUser = (name) => {
+        return updateProfile(auth.currentUser, {
+            displayName:name
+        });
+    }
+
+    const signIn = (email,password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currrentUser => {
+            console.log(currrentUser);
+            setUser(currrentUser);
+            setLoading(false);
+        })
+
+        return ()=>{
+            return unsubscribe();
+        }
+    },[])
+
+    const authInfo = {user, loading, createUser, signIn, logOut, updateUser, googleSignIn};
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
